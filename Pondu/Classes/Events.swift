f61c55b0a9702c0ebab28e2718c6cbe9f9a7b862
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-11-01 02:41:41 -0500

    WIP added event and user classes

---
+++ Pondu/Classes/Events.swift
@@ -0,0 +1,31 @@
+//
+//  Events.swift
+//  Pondu
+//
+//  Created by Jonathan Green on 11/1/15.
+//  Copyright © 2015 Jonathan Green. All rights reserved.
+//
+
+import UIKit
+
+class Event {
+    
+    var objectID:String = ""
+    var eventType:String = ""
+    var school:[String] = []
+    var name:String = ""
+    var post:String = ""
+    var profilePicture:String = ""
+    var video:String = ""
+    var type:String = ""
+    var likes:String = ""
+    var location:String = ""
+    var story:String = ""
+    var mainthumb:String = ""
+    var comments:[String] = []
+    
+    init(theID:String,theEventType:String,theSchool:[String],theName:String,thePost:String,TheProfilePicture:String,theVideo:String,theType:String,theLikes:String,theLocation:String,theStory:String,theMainThumb:String,theComments:String){
+        
+    }
+    
+}
