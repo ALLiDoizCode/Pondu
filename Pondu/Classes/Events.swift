0e26e8ef1eb9fccfd19720ad29df9a2fd028e6ac
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-11-01 12:23:45 -0500

    WIP event class

--- Pondu/Classes/Events.swift
+++ Pondu/Classes/Events.swift
@@ -11,21 +11,31 @@ import UIKit
 class Event {
     
     var objectID:String = ""
-    var eventType:String = ""
-    var school:[String] = []
+    //var eventType:String = ""
+    //var school:[String] = []
     var name:String = ""
     var post:String = ""
     var profilePicture:String = ""
     var video:String = ""
-    var type:String = ""
+    //var type:String = ""
     var likes:String = ""
     var location:String = ""
     var story:String = ""
     var mainthumb:String = ""
     var comments:[String] = []
     
-    init(theID:String,theEventType:String,theSchool:[String],theName:String,thePost:String,TheProfilePicture:String,theVideo:String,theType:String,theLikes:String,theLocation:String,theStory:String,theMainThumb:String,theComments:String){
-        
+    init(theID:String,theName:String,thePost:String,TheProfilePicture:String,theVideo:String,theLikes:String,theLocation:String,theStory:String,theMainThumb:String,theComments:[String]){
+
+        objectID = theID
+        name = theName
+        post = thePost
+        profilePicture = TheProfilePicture
+        video = theVideo
+        likes = theLikes
+        location = theLocation
+        story = theStory
+        mainthumb = theMainThumb
+        comments = theComments
     }
     
 }
