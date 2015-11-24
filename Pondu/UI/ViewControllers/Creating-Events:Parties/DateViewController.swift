91a3e9c0e8ed75ccf85232a101e2195de04c5866
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-11-24 18:20:22 -0500

    WIP end date min date is 30mins past start date

--- Pondu/UI/ViewControllers/Creating-Events:Parties/DateViewController.swift
+++ Pondu/UI/ViewControllers/Creating-Events:Parties/DateViewController.swift
@@ -197,7 +197,7 @@ class DateViewController: UIViewController {
             
             datePickerView.addTarget(self, action: Selector("endTimePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
             
-            datePickerView.minimumDate = eventBegins
+            datePickerView.minimumDate = eventBegins.add(years: 0, months: 0, weeks: 0, days: 0, hours: 0, minutes: 30, seconds: 0)
         }
         
     }
