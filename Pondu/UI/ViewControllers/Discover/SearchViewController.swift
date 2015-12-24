d452c656a1f3f7cb49bc00fd0325883dba9d8c20
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-12-23 21:35:14 -0500

    added message and camera navbaritems

--- Pondu/UI/ViewControllers/Discover/SearchViewController.swift
+++ Pondu/UI/ViewControllers/Discover/SearchViewController.swift
@@ -11,22 +11,45 @@ import UIKit
 class SearchViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate {
     
     let identifier = "Search"
+    let identifier2 = "camera"
     
     lazy   var searchBar:UISearchBar = UISearchBar(frame: CGRectMake(0, 0, 300, 20))
+    
     var searchBtn:UIBarButtonItem!
+    var cameraRightBtn:UIBarButtonItem!
+    var messageRightBtn:UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchBtn = UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: "toggleSearch")
-        self.navigationItem.leftBarButtonItem = searchBtn
+        let messageBtn = UIButton(frame: CGRectMake(0, 0, 20, 20))
+        let cameraBtn = UIButton(frame: CGRectMake(0, 0, 30, 30))
         
+        messageBtn.setImage(UIImage(named: "bubble"), forState: .Normal)
+        messageBtn.addTarget(self, action: "message", forControlEvents: .TouchUpInside)
+        cameraBtn.setImage(UIImage(named: "camera"), forState: .Normal)
+        cameraBtn.addTarget(self, action: "camera", forControlEvents: .TouchUpInside)
+        
+        searchBtn = UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: "toggleSearch")
+        cameraRightBtn = UIBarButtonItem(customView: cameraBtn)
+        messageRightBtn = UIBarButtonItem(customView: messageBtn)
+        self.navigationItem.leftBarButtonItem = searchBtn
+        self.navigationItem.setRightBarButtonItems([cameraRightBtn,messageRightBtn], animated: true)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func camera(){
+        
+        self.performSegueWithIdentifier(identifier2, sender: self)
+    }
+    
+    func message(){
+        
     }
     
     func toggleSearch(){
@@ -71,6 +94,7 @@ class SearchViewController: UIViewController,UITableViewDataSource,UITableViewDe
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
         
         searchBar.hidden = true
+        searchBar.resignFirstResponder()
         self.navigationItem.leftBarButtonItem = searchBtn
         
     }
