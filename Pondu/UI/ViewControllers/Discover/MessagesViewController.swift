718f8535483bb1d17ddb43fd4c91097106387356
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2016-03-04 12:34:33 -0500

    WIP message viewcontroller

--- Pondu/UI/ViewControllers/Discover/MessagesViewController.swift
+++ Pondu/UI/ViewControllers/Discover/MessagesViewController.swift
@@ -73,14 +73,13 @@ class MessagesViewController: UIViewController,UITableViewDataSource,UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        
     }
-    */
-
+    
 }
