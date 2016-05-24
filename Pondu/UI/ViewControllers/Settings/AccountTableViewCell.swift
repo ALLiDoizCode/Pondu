e2b7088799eba62cc84a9a6fb3e53a032c180405
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2016-05-24 16:34:32 -0400

    WIP account settings

---
+++ Pondu/UI/ViewControllers/Settings/AccountTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  AccountTableViewCell.swift
+//  Pondu
+//
+//  Created by Jonathan Green on 5/24/16.
+//  Copyright © 2016 Jonathan Green. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.commonSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonSetup()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        var frame = self.imageView!.frame
+        let imageSize = 20 as CGFloat
+        frame.size.height = imageSize
+        frame.size.width  = imageSize
+        frame.origin.y = frame.origin.y + 10
+        self.imageView!.frame = frame
+        self.imageView!.clipsToBounds = true
+    }
+    
+    func commonSetup() {
+        //self.accessoryCheckmarkColor = UIColor.whiteColor()
+        let backgroundView = UIView(frame: self.bounds)
+        backgroundView.autoresizingMask = [ .FlexibleHeight, .FlexibleWidth ]
+        let backgroundColor = UIColor(red:0.34, green:0.29, blue:0.36, alpha:1.0)
+        backgroundView.backgroundColor = backgroundColor
+        
+        self.backgroundView = backgroundView
+        
+        self.textLabel?.backgroundColor = UIColor.clearColor()
+        self.textLabel?.textColor = UIColor.whiteColor()
+    }
+    
+    func updateContentForNewContentSize() {
+        self.textLabel?.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Pondu/UI/ViewControllers/Settings/AccountViewController.swift
+++ Pondu/UI/ViewControllers/Settings/AccountViewController.swift
@@ -1,46 +1,33 @@
 //
-//  SideViewController.swift
+//  AccountViewController.swift
 //  Pondu
 //
-//  Created by Jonathan Green on 5/13/16.
+//  Created by Jonathan Green on 5/24/16.
 //  Copyright © 2016 Jonathan Green. All rights reserved.
 //
 
 import UIKit
+import Material
+import Cartography
 
-enum DrawerSection: Int {
-    case ViewSelection
-    case DrawerWidth
-    case ShadowToggle
-    case OpenDrawerGestures
-    case CloseDrawerGestures
-    case CenterHiddenInteraction
-    case StretchDrawer
-}
-
-class SideViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    let editProfileVC = EditProfileViewcontroller()
-    let accountSettintsVC = AccountViewController()
+class AccountViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     var tableView: UITableView!
-    let drawerWidths: [CGFloat] = [160, 200, 240, 280, 320]
+    var currentTitle:UILabel!
+    var backBtn:MaterialButton!
     
-    let accountDetails = ["Edit Pofile","Account Settings","Notifications",""]
-    let accoutIcons = ["round","cogwheel","sound",""]
-    let accoutIcon = "key"
-    
-    let legal = ["Help","Terms of Service","Privacy Policy","Pondu-app.com",""]
-    let legalIcons = ["people","tool","security","window",""]
-    let legalIcon = "legal"
+    let sections = ["Prefrences","Privacy/Blocked","Actions"]
+    let prefences = ["Home School"]
+    let privacyBlocked = ["Private Account","Blocked Users"]
+    let actions = ["Clear Messages","Logout","Delete Account"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tableView = UITableView(frame: self.view.frame, style: .Grouped)
+        self.tableView = UITableView(frame: CGRectMake(0, 0, 0, 0), style: .Grouped)
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.view.addSubview(self.tableView)
+        setupUI()
         self.tableView.autoresizingMask = [ .FlexibleWidth, .FlexibleHeight ]
         
         self.tableView.backgroundColor = UIColor.darkGrayColor()
@@ -48,118 +35,93 @@ class SideViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 161 / 255, green: 164 / 255, blue: 166 / 255, alpha: 1.0)
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 55 / 255, green: 70 / 255, blue: 77 / 255, alpha: 1.0)]
-        
-        //self.view.backgroundColor = UIColor.darkGrayColor()
+
+        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
         
-        // See https://github.com/sascha/DrawerController/issues/12
         self.navigationController?.view.setNeedsLayout()
         
         self.tableView.reloadSections(NSIndexSet(indexesInRange: NSRange(location: 0, length: self.tableView.numberOfSections - 1)), withRowAnimation: .None)
     }
     
-     func contentSizeDidChange(size: String) {
-        self.tableView.reloadData()
-    }
-    
-    // MARK: - UITableViewDataSource
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return accountDetails.count
-        case 1:
-            return legal.count
-        default:
-            return 0
+    func setupUI() {
+        
+        currentTitle = UILabel()
+        currentTitle.text = "Account Settings"
+        backBtn = MaterialButton()
+        backBtn.setImage(UIImage(named:"arrows"), forState: UIControlState.Normal)
+        
+        constrain(tableView,currentTitle) { tableView,currentTitle in
+            
+            tableView.left == (tableView.superview?.left)!
+            tableView.height == (tableView.superview?.height)! * 0.93
+            tableView.width == (tableView.superview?.width)!
+            tableView.bottom == (tableView.superview?.bottom)!
         }
     }
     
+    func contentSizeDidChange(size: String) {
+        self.tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return sections.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return prefences.count
+        case 1:
+            return privacyBlocked.count
+        case 2:
+            return actions.count
+        default:
+            break
+        }
+        
+        return 0
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let CellIdentifier = "Cell"
         
         var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as UITableViewCell?
         
         if cell == nil {
-            cell = SideDrawerTableViewCell(style: .Default, reuseIdentifier: CellIdentifier)
+            cell = AccountTableViewCell(style: .Default, reuseIdentifier: CellIdentifier)
             cell.selectionStyle = .Blue
         }
         
-        cell.selectionStyle = UITableViewCellSelectionStyle.None
-        
         switch indexPath.section {
+            
         case 0:
-            
-            cell.textLabel?.text = accountDetails[indexPath.row]
-            cell.imageView?.image = UIImage(named: accoutIcons[indexPath.row])
-            
+            cell.textLabel?.text = prefences[indexPath.row]
         case 1:
-            
-            cell.textLabel?.text = legal[indexPath.row]
-            
-            cell.imageView?.image = UIImage(named: legalIcons[indexPath.row])
-
-            
-        default: break
-            
+            cell.textLabel?.text = privacyBlocked[indexPath.row]
+        case 2:
+            cell.textLabel?.text = actions[indexPath.row]
+        default:
+            break
         }
+        
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
         
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        
-        switch indexPath.section {
-        case 0:
-            
-            switch indexPath.row {
-                
-            case 0:
-                print(accountDetails[0])
-                
-                sideMenuController()?.setContentViewController(editProfileVC)
-            case 1:
-                
-                print(accountDetails[1])
-                
-                sideMenuController()?.setContentViewController(accountSettintsVC)
-                
-            case 3:
-                
-                print(accountDetails[2])
-                
-
-            default:
-                break
-            }
-            
-        case 1:
-            
-            switch indexPath.row {
-            case 0:
-                print(legal[0])
-            case 1:
-                print(legal[1])
-            case 2:
-                print(legal[2])
-            case 3:
-                print(legal[3])
-            default:
-                break
-            }
-            
-        default: break
-            
-        }
     }
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -168,10 +130,12 @@ class SideViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         switch section {
         case 0:
-            return "Account Details"
+            return sections[section]
         case 1:
-            return "Legal"
-
+            return sections[section]
+        case 2:
+            return sections[section]
+            
         default:
             return nil
         }
@@ -220,15 +184,7 @@ class SideViewController: UIViewController, UITableViewDataSource, UITableViewDe
         let header = view as! UITableViewHeaderFooterView
         
         header.textLabel?.textColor = title.textColor
-    
-    }
-
-    
-    // MARK: - UITableViewDelegate
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
